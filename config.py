8b425e53c6fa5e7d01af6b708c91f78c83d49db3
Marco Zingales <marcozingales@Marcos-MacBook-Pro.local>
2020-12-12 04:12:50 +0100

    removed private keys

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 mailgun_api_url = 'http://api.mailgun.net/v2/sandboxb7a447390a814952b490dd14bbfdbbda.mailgun.org/messages'
-mailgun_key = '50c00dfe5e12fa4a9637a71476676a29-2af183ba-6f8e54ec'
+mailgun_key = 'some-key'
 
-sendgrid_key = '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'
+sendgrid_key = 'some-other-key'
 sendgrid_url = 'https://api.sendgrid.com/v3/'
